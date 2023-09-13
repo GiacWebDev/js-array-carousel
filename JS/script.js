f07a5d1544ffb66fa89b5db8f5e87218159004ee
GiacWebDev <giacobechini@gmail.com>
2023-09-13 19:02:52 +0200

    [edit] js hide class

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,11 @@
 
 // elementi presi da HTML
 const itemsWrapper = document.querySelector('.items-wrapper');
+const btnNext = document.querySelector('.next');
+const btnPrev = document.querySelector('.prev');
 
+
+// salvo le immagini in un array
 const listaImmagini = [
   'img/01.webp',
   'img/02.webp',
@@ -10,14 +14,27 @@ const listaImmagini = [
   'img/05.webp'
 ];
 
+// quando un item è pari a 0 poi toglierò classe hide
 let imgCounter = 0
 
 // creo ciclo immagini
 for (i = 0; i < listaImmagini.length; ++i) {
+  // prendo un'immagine singola
   const image = listaImmagini[i];
+
+  console.log(image);
+  // stampo in pagina dentro wrapper x il numero della lunghezza dell'array
   itemsWrapper.innerHTML += `<img src="${image}" class="hide item" alt="">`;
 }
 
-const itemCollection = document.getElementById('item');
+// prendi tutti gli elementi che hanno item come classe
+const itemCollection = document.getElementsByClassName('item');
+console.log(itemCollection);
 
-itemCollection[imgCounter].classlist.remove('hide');
+// imposto la classe hide al counter delle img in modo da applicarsi in automatico grazie al ciclo
+itemCollection[imgCounter].classList.remove('hide');
+
+btnNext.addEventListener('click', function(){
+
+});
+
