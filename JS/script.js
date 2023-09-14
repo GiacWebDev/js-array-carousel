a2f167a33503cd1d39f22191147ed84afc4551fa
GiacWebDev <giacobechini@gmail.com>
2023-09-14 10:33:08 +0200

    [edit] btn loop (still hide btn bug)

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,9 @@
 
+
 // elementi presi da HTML
 const itemsWrapper = document.querySelector('.items-wrapper');
-const btnUp = document.querySelector('.up');
-const btnDown = document.querySelector('.down');
+const btnUp =        document.querySelector('.btn-arrow.up');
+const btnDown =      document.querySelector('.btn-arrow.down');
 
 
 // salvo le immagini in un array
@@ -14,39 +15,48 @@ const listaImmagini = [
   'img/05.webp'
 ];
 
-// quando un item è pari a 0 poi toglierò classe hide
-let imgCounter = 0
+btnUp.classList.add('hide');
+
+// counter parte da 0
+let imgCounter = 0;
+
 
 // creo ciclo immagini
 for (let i = 0; i < listaImmagini.length; ++i) {
-  // prendo un'immagine singola
+  // creo variabile per il risultato del ciclo = image
   const image = listaImmagini[i];
-
-  console.log(image);
   // stampo in pagina dentro wrapper x il numero della lunghezza dell'array
   itemsWrapper.innerHTML += `<img src="${image}" class="hide item" alt="">`;
 }
 
 // prendi tutti gli elementi che hanno item come classe
 const itemCollection = document.getElementsByClassName('item');
-
-// imposto la classe hide al counter delle img in modo da applicarsi in automatico grazie al ciclo
+// // e gli rimuovo la classe hide
 itemCollection[imgCounter].classList.remove('hide');
+
 
 btnDown.addEventListener('click', function(){
   // nascondo ila img al click
   itemCollection[imgCounter].classList.add('hide');
   // quanto il counter avanza di 1 riattivo la visibilità rimuovendo hide
-  imgCounter++
+  imgCounter++;
+
+  if (imgCounter === listaImmagini.length) imgCounter = 0;
+  
   itemCollection[imgCounter].classList.remove('hide')
+  
 });
 
 btnUp.addEventListener('click', function(){
+  
   itemCollection[imgCounter].classList.add('hide');
- 
-  imgCounter--
+  
+  imgCounter--;
+
+  if (imgCounter < 0) imgCounter = listaImmagini.length - 1;
+  
   itemCollection[imgCounter].classList.remove('hide')
+  
 });
 
-console.log(itemCollection);
 
